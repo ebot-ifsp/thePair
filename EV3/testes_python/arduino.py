5c111abd62582b061ec72560fb424d9b4ce8d40c
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2018-06-11 12:43:23 -0300

    First commit
    Arquivos do i2c arduino e biblioteca python i2c para os sensores IF

---
+++ EV3/testes_python/arduino.py
@@ -0,0 +1,39 @@
+# -*- coding: latin-1 -*-
+'''
+http://wiki.erazor-zone.de/wiki:linux:python:smbus:doc
+
+
+
+'''
+
+import sys, os
+
+from smbus import SMBus
+
+class Arduino():
+    def __init__(self):
+        self.address = 0x04
+        self.canal = 6
+        self.pedido = 1
+        self.led_on = 2
+        self.led_off = 3
+        self.tamanho = 8
+        self.buss = SMBus(self.canal)
+
+    def le_todos(self):
+        resposta =  self.buss.read_i2c_block_data(
+                    self.address, self.pedido, self.tamanho)
+        return resposta
+    
+    def le(self, qual):
+        valores = self.le_todos()
+        return valores[qual]
+
+    def liga_led(self):
+        resposta = self.buss.read_i2c_block_data(
+                    self.address, self.led_on, self.tamanho)
+
+    def desliga_led(self):
+        self.buss.read_i2c_block_data(
+                    self.address, self.led_off, self.tamanho)
+
