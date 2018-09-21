b5e037e17f66a53a6c5264c42bba898b1f1c1bdd
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2018-09-21 00:59:45 -0300

    organizacao
    Alteracao na funcao gira - com opcao dde block e sem giroscopio

---
+++ testes_antigos/Antigo2/testes_python/ninja.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+import ev3dev.ev3 as ev3
+from controle import *
+from arduino import *
+import numpy as np
+import time
+
+ard = Arduino()
+estado = 1
+
+while 1:
+    if ir.value() < minimo_if:                     # caso chegue no obstaculo
+        desvia_bloco()
+    sensores = ard.sensores()
+    qt_preto = np.count_nonzero(sensores)
+    if qt_preto == 0:
+        anda(v_reta + 200, 0)
+    elif qt_preto > 3:                      # encruzilhada ou virada (verde)
+        print('>3', qt_preto)
+        acao = sequencia[0]
+        sequencia.pop(0)
+        if acao == 'esq':
+            print('esq')
+            curvaVerde('esquerda', ard)
+        elif acao == 'dir':
+            print('dir')
+            curvaVerde('direita', ard)
+        elif acao == 'vol':
+            print('vol')
+            voltar(ard)
+        else:
+            anda_posicao(80)
+            estado = segue_linha(sensores, estado)
+        estado = 1
+    elif qt_preto < 4:
+        estado = segue_linha(sensores, estado)
+
